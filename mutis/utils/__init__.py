95d525d31c85597cb249fca71b4f9a532f9a34ea
Jose Enrique Ruiz <jer@iaa.es>
2021-05-12 20:38:14 +0200

    add utils

---
+++ mutis/utils/__init__.py
@@ -0,0 +1,11 @@
+# Licensed under a 3-clause BSD style license - see LICENSE
+"""
+Utility functions and classes used throughout MUTIS.
+
+You have to import sub-modules of `mutis.utils` directly,
+the `mutis.utils` namespace is empty.
+
+Examples::
+
+    from mutis.utils.utils import memoize
+"""
