4f87d296693de251a370768efcadd17c2342ec25
Jose Enrique Ruiz <jer@iaa.es>
2021-05-13 14:40:50 +0200

    refactor utils

--- mutis/utils/utils.py
+++ mutis/utils/utils.py
@@ -3,7 +3,7 @@
 
 import numpy as np
 
-__all__ = ["get_grid", "memoize", "nindcf"]
+__all__ = ["get_grid", "memoize"]
 
 
 def memoize(f):
@@ -25,19 +25,5 @@ def get_grid(x, y):
     """Compute a meshgrid with memoization."""
     return np.meshgrid(x, y)
 
-
-def nindcf(t1, s1, t2, s2):
-    """Implement normalization and interpolation over numpy correlate function."""
-    # numpy correlate function is not designed for unevenly spaced data
-    # correlation C(tau), where tau goes from
-    # -( np.max([t1.max(),t2.max()]) - np.min([t1.min(),t2.min()]) )
-    # to
-    # +( np.min([t1.max(),t2.max()]) - np.max([t1.min(),t2.min()]) )
-
-    s1i = np.interp(np.linspace(t1.min(), t1.max(), t1.size), t1, s1)
-    s2i = np.interp(np.linspace(t2.min(), t2.max(), t2.size), t2, s2)
-    x = (s1i - np.mean(s1i)) / np.std(s1i) / len(s1i)
-    y = (s2i - np.mean(s2i)) / np.std(s2i)
-    return np.correlate(x, y, 'full')
 
 
