14d4856cb3084a02d7a798850dbc002f4839af98
Ruben Lopez Coto <rlopezcoto@gmail.com>
2021-05-14 10:10:53 +0200

    add classes

--- mutis/__init__.py
+++ mutis/__init__.py
@@ -1,6 +1,10 @@
 # Licensed under a 3-clause BSD style license - see LICENSE
 """MUTIS: A Python package for muti-wavelength time series analysis."""
 
+from . import correlation
+from . import signal
+from . import utils
+
 __all__ = ["__version__"]
 
 import pkg_resources
@@ -10,3 +14,5 @@ try:
 except pkg_resources.DistributionNotFound:
     # package is not installed
     pass
+
+
