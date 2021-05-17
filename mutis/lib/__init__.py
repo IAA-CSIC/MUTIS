f89e672cb4f961d1ee44f51dc2025e47c0d49aa5
Jose Enrique Ruiz <jer@iaa.es>
2021-05-17 17:52:36 +0200

    fix rtd docs built

---
+++ mutis/lib/__init__.py
@@ -0,0 +1,6 @@
+# Licensed under a 3-clause BSD style license - see LICENSE
+"""Utility functions and classes used throughout MUTIS."""
+
+from .correlation import *
+from .signal import *
+from .utils import *
