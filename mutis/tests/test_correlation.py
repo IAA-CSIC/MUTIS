7ebf0327d3fa101b2916e1382d4047f3271d578a
Jose Enrique Ruiz <jer@iaa.es>
2021-05-19 21:36:36 +0200

    improve correlation tests

--- mutis/tests/test_correlation.py
+++ mutis/tests/test_correlation.py
@@ -5,18 +5,40 @@ from mutis.correlation import Correlation
 
 
 @pytest.fixture
-def correlation():
+def corr():
     times = np.linspace(10, 20, 100)
     values = 0.5 * np.sin(1 * np.linspace(10, 20, 100)) + 0.5 * np.sin(6 * np.linspace(10, 20, 100)) + 1
     signal1 = Signal(times, values, "lc_gen_samp")
     signal2 = Signal(times, values, "lc_gen_psd_nft")
-    return Correlation(signal1, signal2, "welsh_ab")
+    return {
+        "fail": Correlation(signal1, signal2, "test"),
+        "welsh_ab": Correlation(signal1, signal2, "welsh_ab"),
+        "kroedel_ab": Correlation(signal1, signal2, "kroedel_ab"),
+        "numpy": Correlation(signal1, signal2, "numpy")
+    }
 
 
-def test_gen_synth(correlation):
-    correlation.gen_synth(10)
+def test_gen_synth(corr):
+    corr["welsh_ab"].gen_synth(10)
 
 
-def test_plot_signals(correlation):
-    correlation.plot_signals()
+def test_plot_signals(corr):
+    corr["welsh_ab"].plot_signals()
+
+
+def test_gen_times(corr):
+    corr["welsh_ab"].gen_times()
+    # TODO assert values
+
+# def test_gen_corr(correlation):
+#     correlation.gen_times()
+#     correlation.gen_corr()
+#
+#
+# def test_plot_corr(correlation):
+#     correlation.gen_times()
+#     correlation.gen_corr()
+#     correlation.plot_corr()
+
+
 
--- mutis/tests/test_signal.py
+++ mutis/tests/test_signal.py
@@ -10,7 +10,7 @@ def signal():
     values = 0.5 * np.sin(1 * np.linspace(10, 20, 100)) + 0.5 * np.sin(6 * np.linspace(10, 20, 100)) + 1
 
     return {
-        "test": Signal(times, values, "test"),
+        "fail": Signal(times, values, "test"),
         "samp": Signal(times, values, "lc_gen_samp"),
         "psd_nft": Signal(times, values, "lc_gen_psd_nft"),
         "psd_fft": Signal(times, values, "lc_gen_psd_fft"),
@@ -30,7 +30,7 @@ def ou_params():
 
 def test_gen_synth(signal, ou_params):
     with pytest.raises(Exception):
-        signal["test"].gen_synth(10)
+        signal["fail"].gen_synth(10)
     signal["samp"].gen_synth(10)
     signal["psd_nft"].gen_synth(10)
     signal["psd_fft"].gen_synth(10)
@@ -46,7 +46,7 @@ def test_gen_synth(signal, ou_params):
 
 def test_psd_checks_gen(signal):
     with pytest.raises(Exception):
-        signal["test"].PSD_check_gen(10)
+        signal["fail"].PSD_check_gen(10)
     signal["psd_nft"].PSD_check_gen()
     signal["psd_fft"].PSD_check_gen()
     signal["psd_lombscargle"].PSD_check_gen()
