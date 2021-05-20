9cbba0512c2d9fd0a686409ab9546981ef3c0925
Jose Enrique Ruiz <jer@iaa.es>
2021-05-20 17:40:19 +0200

    add final correlation tests

--- mutis/tests/test_correlation.py
+++ mutis/tests/test_correlation.py
@@ -6,15 +6,19 @@ from mutis.correlation import Correlation
 
 @pytest.fixture
 def corr():
-    times = np.linspace(10, 20, 100)
-    values = 0.5 * np.sin(1 * np.linspace(10, 20, 100)) + 0.5 * np.sin(6 * np.linspace(10, 20, 100)) + 1
-    signal1 = Signal(times, values, "lc_gen_samp")
-    signal2 = Signal(times, values, "lc_gen_psd_nft")
+    times1 = np.linspace(2, 6, 40)
+    values1 = np.sin(times1)
+    times2 = np.linspace(8, 12, 40)
+    values2 = np.sin(times2)
+    signal1 = Signal(times1, values1, "lc_gen_psd_nft")
+    signal2 = Signal(times2, values2, "lc_gen_psd_nft")
     return {
         "fail": Correlation(signal1, signal2, "test"),
         "welsh_ab": Correlation(signal1, signal2, "welsh_ab"),
         "kroedel_ab": Correlation(signal1, signal2, "kroedel_ab"),
-        "numpy": Correlation(signal1, signal2, "numpy")
+        "numpy": Correlation(signal1, signal2, "numpy"),
+        "fwelsh": Correlation(signal1, signal2, "fwelsh"),
+        "fkroedel": Correlation(signal1, signal2, "fkroedel"),
     }
 
 
@@ -28,17 +32,29 @@ def test_plot_signals(corr):
 
 def test_gen_times(corr):
     corr["welsh_ab"].gen_times()
-    # TODO assert values
-
-# def test_gen_corr(correlation):
-#     correlation.gen_times()
-#     correlation.gen_corr()
-#
-#
-# def test_plot_corr(correlation):
-#     correlation.gen_times()
-#     correlation.gen_corr()
-#     correlation.plot_corr()
+    corr["welsh_ab"].gen_times(ftimes="rawab")
+    corr["welsh_ab"].gen_times(ftimes="uniform")
+    corr["welsh_ab"].gen_times(ftimes="numpy")
+    # TODO assert values ?
 
 
+def test_plot_times(corr):
+    corr["welsh_ab"].gen_times(dtmin=0.1, dtmax=3, nbinsmin=3)
+    corr["welsh_ab"].plot_times(rug=True)
 
+
+def test_gen_corr(corr):
+    corr["welsh_ab"].gen_times(dtmin=0.1, dtmax=3, nbinsmin=3)
+    corr["welsh_ab"].gen_synth(10)
+    corr["welsh_ab"].gen_corr()
+
+    corr["kroedel_ab"].gen_times(dtmin=0.1, dtmax=3, nbinsmin=3)
+    corr["kroedel_ab"].gen_synth(10)
+    corr["kroedel_ab"].gen_corr()
+
+
+def test_plot_corr(corr):
+    corr["welsh_ab"].gen_times(dtmin=0.1, dtmax=3, nbinsmin=3)
+    corr["welsh_ab"].gen_synth(10)
+    corr["welsh_ab"].gen_corr()
+    corr["welsh_ab"].plot_corr()
