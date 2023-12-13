ae404850a9ace0c66871ff729f6be359fa4d0937
Juan Escudero Pedrosa <jescudero@iaa.es>
2023-12-13 12:02:53 +0100

    bump python version to 3.11

--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.11',
 )
